4b5b6a0d6f1f448760d394222ad216c9b0a01b74
hyunul <jungwj1023@naver.com>
2025-05-08 12:50:34 +0900

    mapper 인터페이스 파라미터 수정

--- be/src/main/java/com/aisg/devlogix/mapper/ScheduleMapper.java
+++ be/src/main/java/com/aisg/devlogix/mapper/ScheduleMapper.java
@@ -4,9 +4,8 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface ScheduleMapper {
-    List<Map<String, Object>> getAllSchedule(@Param("user") String user, @Param("startDate") String startDate, @Param("endDate") String endDate, @Param("gubun") String gubun);
+    List<Map<String, Object>> getAllSchedule(String user, String startDate, String endDate, String gubun);
 }
--- be/src/main/java/com/aisg/devlogix/mapper/UserMapper.java
+++ be/src/main/java/com/aisg/devlogix/mapper/UserMapper.java
@@ -4,9 +4,8 @@ import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface UserMapper {
-    List<Map<String, Object>> getUserProfile(@Param("username") String username);
+    List<Map<String, Object>> getUserProfile(String username);
 }
